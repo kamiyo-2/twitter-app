70bc83a24a7fcf3557651de476fe9c3a7d26b7a2
Kamiyo-2 <k.up.kiti.hara.2@gmail.com>
2023-04-27 15:12:10 +0900

    createとeditを一つのformに変更

--- components/TweetForm.tsx
+++ components/TweetForm.tsx
@@ -1,27 +1,48 @@
+import { useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Tweet } from "@/types";
 
 interface TweetFormProps {
   tweets: Tweet[];
   setTweets: (tweets: Tweet[]) => void;
+  editingTweet?: Tweet | null;setEditingTweet
+  : (tweet: Tweet | null) => void;
+  onEditCancel: () => void;
 }
 
 type TweetFormData = Omit<Tweet, "id">;
 
-const TweetForm = ({ tweets, setTweets }: TweetFormProps) => {
-  const { register, handleSubmit, setValue , formState: { errors } } = useForm<TweetFormData>();
+const TweetForm = ({ tweets, setTweets, editingTweet, setEditingTweet, onEditCancel }: TweetFormProps) => {
+  const { register, handleSubmit, setValue, reset, formState: { errors } } = useForm<TweetFormData>();
+
+  useEffect(() => {
+    if (editingTweet) {
+      setValue("text", editingTweet.text);
+    } else {
+      reset();
+    }
+  }, [editingTweet, setValue, reset]);
 
   const onSubmit: SubmitHandler<TweetFormData> = (data) => {
     const timestamp = new Date().toISOString();
 
-    const newTweet: Tweet = {
-      id: Date.now(),
-      ...data,
-      created_at: timestamp,
-      updated_at: timestamp,
-    };
-
-    setTweets([...tweets, newTweet]);
+    if (editingTweet) {
+      const updatedTweet: Tweet = {
+        ...editingTweet,
+        ...data,
+        updated_at: timestamp,
+      };
+      setTweets(tweets.map((tweet) => (tweet.id === updatedTweet.id ? updatedTweet : tweet)));
+      setEditingTweet(null);
+    } else {
+      const newTweet: Tweet = {
+        id: Date.now(),
+        ...data,
+        created_at: timestamp,
+        updated_at: timestamp,
+      };
+      setTweets([...tweets, newTweet]);
+    }
     setValue("text", "");
   };
 
@@ -33,7 +54,8 @@ const TweetForm = ({ tweets, setTweets }: TweetFormProps) => {
       />
       {errors.text && errors.text.type === "required" && <span>このフィールドは必須です</span>}
       {errors.text && errors.text.type === "maxLength" && <span>最大文字数は280文字です</span>}
-      <button type="submit">Tweet</button>
+      <button type="submit">{editingTweet ? "Update" : "Tweet"}</button>
+      {editingTweet && <button type="button" onClick={onEditCancel}>Cancel</button>}
     </form>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,42 @@
-import {useState } from "react";
+import { useState } from "react";
 import { Tweet } from "@/types";
 import TweetForm from "@/components/TweetForm";
-import TweetItem from "@/components/Tweet";
-
 
 export default function Home() {
   const [tweets, setTweets] = useState<Tweet[]>([]);
+  const [editingTweet, setEditingTweet] = useState<Tweet | null>(null);
 
   const deleteTweet = (id: number) => {
     setTweets(tweets.filter((tweet) => tweet.id !== id));
   };
 
-  const updateTweet = (updatedTweet: Tweet) => {
-    setTweets(tweets.map((tweet) => (tweet.id === updatedTweet.id ? updatedTweet : tweet)));
+  const startEditing = (id: number) => {
+    const tweetToEdit = tweets.find((tweet) => tweet.id === id);
+    if (tweetToEdit) {
+      setEditingTweet(tweetToEdit);
+    }
+  };
+
+  const cancelEditing = () => {
+    setEditingTweet(null);
   };
 
   return (
     <div>
-      <TweetForm tweets={tweets} setTweets={setTweets} />
-      {tweets.map((tweet) => (
-        <TweetItem
-        key={tweet.id}
-        tweet={tweet}
-        onDelete={deleteTweet}
-        onUpdate={updateTweet}
+      <TweetForm
+      tweets={tweets}
+      setTweets={setTweets}
+      editingTweet={editingTweet}
+      setEditingTweet={setEditingTweet}
+      onEditCancel={cancelEditing}
       />
+      {tweets.map((tweet) => (
+        <div key={tweet.id}>
+        <p>{tweet.text}</p>
+        <button onClick={() => deleteTweet(tweet.id)}>Delete</button>
+        <button onClick={() => startEditing(tweet.id)}>Edit</button>
+      </div>
       ))}
     </div>
   );
-
 };
